446fac69d87cdec88d67e92deaae8cad3eb850f7
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2025-01-27 16:37:23 +0700

    feat: generate Filament resource for City API management

---
+++ app/Filament/Resources/Api/CityResource/Pages/CreateCity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\Api\CityResource\Pages;
+
+use App\Filament\Resources\Api\CityResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCity extends CreateRecord
+{
+    protected static string $resource = CityResource::class;
+}
