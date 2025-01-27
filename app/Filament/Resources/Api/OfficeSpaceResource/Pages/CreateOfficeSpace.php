1fe15384eabba47adfcf2a4e41786716e1957aaa
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2025-01-27 16:37:41 +0700

    feat: generate Filament resource for OfficeSpace API management

---
+++ app/Filament/Resources/Api/OfficeSpaceResource/Pages/CreateOfficeSpace.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\Api\OfficeSpaceResource\Pages;
+
+use App\Filament\Resources\Api\OfficeSpaceResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateOfficeSpace extends CreateRecord
+{
+    protected static string $resource = OfficeSpaceResource::class;
+}
