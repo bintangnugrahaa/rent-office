f5b1625c00f55df804ec955524e09b963080ed00
bintangnugraha <djakarta1973@gmail.com>
2025-01-29 11:03:43 +0700

    feat: integrate API data fetching into BrowseOfficeWrapper component

--- frontend/src/wrappers/BrowseOfficeWrapper.tsx
+++ frontend/src/wrappers/BrowseOfficeWrapper.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
 import OfficeCard from "../components/OfficeCard";
+import axios from "axios";
+import { Office } from "../types/type";
 
 export default function BrowseOfficeWrapper() {
+  const [offices, setCities] = useState<Office[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/offices", {
+        headers: {
+          
+        },
+      })
+      .then((response) => {
+        setCities(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading data: {error}</p>;
+  }
+
   return (
     <section
       id="Fresh-Space"
@@ -12,7 +44,9 @@ export default function BrowseOfficeWrapper() {
         For Your Better Productivity.
       </h2>
       <div className="grid grid-cols-3 gap-[30px]">
-        <OfficeCard></OfficeCard>
+        {offices.map((office) => (
+          <OfficeCard key={office.id} office={office}></OfficeCard>
+        ))}
       </div>
     </section>
   );
